9978f6c86a71a272d65d0f9893f48fc7fc99da3c
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-08-28 19:20:03 -0300

    Code 35 Added

---
+++ 35-digital-calculator/script.js
@@ -0,0 +1,54 @@
+const output = document.querySelector('.container header input')
+const keys = document.querySelector('.container .keys')
+
+const labels = [
+    { value: '+', color: 'orange' },
+    { value: '-', color: 'purple' },
+    { value: '*', color: 'navy' },
+    { value: '/', color: 'green' },
+    { value: 'C', color: 'black' },
+    { value: '=', color: 'red' },
+    { value: '-1', color: 'gray' }
+]
+
+for (let i = 0; i < 10; i++) {
+    let number = document.createElement('button');
+    number.classList.add('number');
+    number.innerHTML = i;
+    number.addEventListener('click', (e) => addValue(e.target.innerHTML))
+
+    keys.appendChild(number)
+}
+
+labels.map(item => {
+    let operator = document.createElement('button');
+    operator.classList.add('operator');
+    operator.innerHTML = item.value;
+    operator.style.backgroundColor = item.color;
+
+    if (item.value === '=' ) {
+        operator.addEventListener('click', () => evalOperation());
+        keys.appendChild(operator);
+        return;
+    } else if (item.value === 'C') {
+        operator.addEventListener('click', () => output.value = '');
+        keys.appendChild(operator);
+        return;
+    } else if (item.value === '-1') {
+        operator.addEventListener('click', () => output.value = output.value.slice(0, -1))
+        keys.appendChild(operator)
+        return;
+    }
+    
+    operator.addEventListener('click', (e) => addValue(e.target.innerHTML));
+    keys.appendChild(operator);
+})
+
+const addValue = target => output.value += target;
+function evalOperation() {
+    if (output.value === '') {
+        return;
+    }
+    const result = eval(output.value)
+    output.value = result
+}
