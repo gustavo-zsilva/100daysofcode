98e4e5dd65388c0d11aa4df2905cedf7f91b5fdd
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-08-28 17:07:35 -0300

    Finished Code 37

--- 37-event-countdown-timer/script.js
+++ 37-event-countdown-timer/script.js
@@ -1,9 +1,72 @@
-const dataDiv = document.querySelector('.container .data-div')
+const container = document.querySelector('.container');
+const dataDiv = document.querySelector('.container .date-div')
 const timeDiv = document.querySelector('.container .time-div')
+const newDateBtn = document.querySelector('.container .add-date')
 
-let date = new Date('Feb 2, 2020 12:00:00').getDate()
+const modal = document.querySelector('.modal')
+const newDateInput = document.querySelector('.modal input')
 
-// let dateArray = date.toLocaleDateString().split('/')
-// let timeArray = date.toLocaleTimeString().split(':')
+let countDownFromDate = new Date(localStorage.getItem('timer_date')).toDateString()
+let countDownDate = new Date(localStorage.getItem('timer_date')).getTime()
 
-console.log(date)
+
+dataDiv.innerHTML = countDownFromDate
+
+newDateBtn.addEventListener('click', () => {
+    const quitBtn = document.querySelector('.modal img')
+    const modalAddBtn = document.querySelector('.modal button')
+
+    handleModalActions('remove', 'blur(8px)')
+
+    quitBtn.addEventListener('click', () => handleModalActions('add'))
+
+    modalAddBtn.addEventListener('click', () => {
+        if (checkInput()) {
+            alert('Digite uma data válida.')
+            return;
+        }
+
+        countDownFromDate = new Date(newDateInput.value).toDateString()
+        countDownDate = new Date(newDateInput.value).getTime()
+        dataDiv.innerHTML = countDownFromDate
+        initTimer()
+        saveToStorage()
+        handleModalActions('add')
+    })
+})
+
+function initTimer() {
+    let intervalID = setInterval(() => {
+        let now = new Date().getTime()
+    
+        // Distância entre data atual e data marcada
+        let distance = countDownDate - now;
+    
+        // Pegar dias, horas, minutos e segundos
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+        timeDiv.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        
+        if (distance < 0) {
+            clearInterval(intervalID);
+            timeDiv.innerHTML = 'Timer Expired!';
+        }
+
+    }, 1000)
+}
+
+function saveToStorage() {
+    localStorage.setItem('timer_date', newDateInput.value)
+}
+
+function handleModalActions(act, blur = '') {
+    act === 'add' ? modal.classList.add('hide') : modal.classList.remove('hide');
+    container.style.filter = blur;
+}
+
+const checkInput = () => newDateInput.value.length === 0 ? true : false;
+
+initTimer()
